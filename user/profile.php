a442ee19f03749100995fb589a7b82a10f43026d
Aravinth Ravichandiran <aravinthnirmal98@gmail.com>
2021-09-09 03:48:12 +0100

    Changes to update route
    - Create profile.php under user
    - can get all updates for an user
    - added is_admin()
    -  can get all updates from the database

---
+++ user/profile.php
@@ -0,0 +1,12 @@
+<?php
+    /**
+     * PHP file fetching and editing the profile details
+     */
+
+    header('Access-Control-Allow-Methods: GET POST PUT');
+    header('Content-Type: application/json');
+
+    
+
+
+?>
--- utilities/user.php
+++ utilities/user.php
@@ -3,20 +3,20 @@
     // Verify if the user is authenticated
     function verify_user($conn, $user_id, $api_key) {
 
-        $VERIFY_USER_QUERY = "SELECT USERID FROM TC_user WHERE USERID = {$user_id} AND api_key = '{$api_key}'";
+
+        $VERIFY_USER_QUERY = "SELECT USERID FROM TC_user WHERE USERID = ${user_id} AND api_key = '${api_key}'";
 
         $verified_user_id = select_query($conn, $VERIFY_USER_QUERY);
 
-        if($verified_user_id -> num_rows == 0) {
-            return false;
-        } else if($verified_user_id -> num_rows == 1) {
+        if($verified_user_id -> num_rows == 1) {
             return true;
-        } else 
-            return false;
+        } 
+        
+        return false;
     }
 
     function get_usertypeid($conn, $type) {
-        $usertypeid_Query = "SELECT USERTYPEID FROM TC_userType WHERE UserType = '{$type}'\"";
+        $usertypeid_Query = "SELECT USERTYPEID FROM TC_userType WHERE UserType = \"{$type}\"";
         $res_usertypeid = select_query($conn, $usertypeid_Query);
 
         if($res_usertypeid -> num_rows > 0) {
@@ -31,5 +31,20 @@
         if($usertype->num_rows > 0) {
             return $usertype->fetch_assoc()['UserType'];
         }
+    }
+
+    function verify_admin($conn, $user_id) {
+
+        // get admins user type id
+        $admin_usertypeid = get_usertypeid($conn, "Admin");
+
+        $VERIFY_ADMIN_QUERY = "SELECT USERID FROM TC_user WHERE USERID = $user_id AND USERTYPEID = $admin_usertypeid";
+        $verified_admin_id = select_query($conn, $VERIFY_ADMIN_QUERY);
+
+        if($verified_admin_id -> num_rows = 1) {
+            return true;
+        }
+
+        return false;
     }
 ?>
